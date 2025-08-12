918e843793dcc50240bb5794e2b1ac469852e950
Manu Farfaro <manu.farfaro@gmail.com>
2025-08-12 00:02:01 -0300

    feat: update to pattern matching style

--- src/main.rs
+++ src/main.rs
@@ -7,14 +7,16 @@ enum Media {
 
 impl Media {
     fn description(&self) -> String {
-        if let Media::Audiobook { title } = self {
-            format!("Audiobook: {}", title)
-        } else if let Media::Movie { title, director } = self {
-            format!("Movie: {}, {}", title, director)
-        } else if let Media::Book { title, author } = self {
-            format!("Book: {}, {}", title, author)
-        } else {
-            String::from("Type not supported")
+        match self {
+            Media::Book { title, author } => {
+                format!("Book: {}, {}", title, author)
+            }
+            Media::Movie { title, director } => {
+                format!("Movie: {}, {}", title, director)
+            }
+            Media::Audiobook { title } => {
+                format!("Audiobook: {}", title)
+            }
         }
     }
 }
